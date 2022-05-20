a7f2ee2a283d470153a52f003070b63a9ed8f0ba
Turku Karakus <turkukarakus1@gmail.com>
2022-05-20 16:04:51 +0300

    Added score tmp
    Added script for score
    
    Created prefab for mama and scorecanvas

--- Assets/Scripts/Mama.cs
+++ Assets/Scripts/Mama.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Mama : MonoBehaviour
 {
+
+    [SerializeField] private TextMeshProUGUI _text;
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Mama"))
+
         {
-            Destroy(gameObject);
+            Destroy(other.gameObject);
+
+            Score.totalScore++;
+
+            _text.text = Score.totalScore.ToString();
+
+            
+
         }
     }
 }
