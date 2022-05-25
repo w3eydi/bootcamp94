bc0c73536fda4f55f53ddb2e5cfb60d3965e2b14
Turku Karakus <turkukarakus1@gmail.com>
2022-05-25 20:12:15 +0300

    Changed game name Added mute button Added credits button Added gameover scene Added mute script

---
+++ Assets/Scripts/Mute.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mute : MonoBehaviour
+{
+    public void MuteToggle(bool muted)
+
+    {
+
+        if(muted)
+
+        {
+
+            AudioListener.volume = 0;
+        
+        }
+
+        else
+
+        {
+
+            AudioListener.volume = 1;
+        }
+
+
+
+    }
+}
