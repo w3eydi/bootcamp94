d56354a0d6b7e5f6839454071c47b40b65424fdb
w3eydi <w3.eydi@gmail.com>
2022-05-31 20:31:49 +0300

    Added NextLevel prefab.
     - Bug fixes and scene ordered.

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    // Jump to Next Level
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.name == "Character" && 
+        SceneManager.GetActiveScene().name == "Level1"){
+            SceneManager.LoadScene("Level2");
+        } else if (other.gameObject.name == "Character" && 
+        SceneManager.GetActiveScene().name == "Level2"){
+            SceneManager.LoadScene("BossLevel");
+        }
+    }
+}
