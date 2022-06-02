b9a51f1f38a0e674ff8c8a0a409f815524c9b686
Suzan Sezgin <suzansezgin09@gmail.com>
2022-06-02 17:50:55 +0300

    audio added next level

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -10,9 +10,11 @@ public class NextLevel : MonoBehaviour
         if (other.gameObject.name == "Character" && 
         SceneManager.GetActiveScene().name == "Level1"){
             SceneManager.LoadScene("Level2");
+            GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>().ComplateAudio();
         } else if (other.gameObject.name == "Character" && 
         SceneManager.GetActiveScene().name == "Level2"){
             SceneManager.LoadScene("BossLevel");
+            GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>().ComplateAudio();
         }
     }
 }
