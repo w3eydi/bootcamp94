06ed36b4adbd553d1d69924a0ff8e490e8740cd2
w3eydi <w3.eydi@gmail.com>
2022-06-05 11:16:53 +0300

    Added scoreText code.

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
     [SerializeField] GameObject EndofScene;
     [SerializeField] Canvas _touchCanvas;
+    [SerializeField] TextMeshProUGUI _scoreText;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.name == "Character") {
             Time.timeScale = 0f;
             EndofScene.SetActive(true);
             _touchCanvas.enabled = false;
+            _scoreText.text = "Your Score : " + Score.totalScore.ToString();
         }
     }
 
