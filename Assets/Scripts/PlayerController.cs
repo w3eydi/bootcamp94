290484374980f36fb0f1eccabcd707b706ed37eb
w3eydi <w3.eydi@gmail.com>
2022-06-06 19:04:12 +0300

    Added orthographicSize feature to MainCamera.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -93,7 +93,7 @@ public class PlayerController : MonoBehaviour
             isDamaged = false;
         } else if (other.gameObject.name == "BossTrigger") {
             Score.bossTrigger = true;
-            Debug.Log("tetiklendi");
+            Camera.main.orthographicSize = 10;
         }
     }
 }
