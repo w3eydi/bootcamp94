1fc4e206376806042d3b68649fb208621fea4742
w3eydi <w3.eydi@gmail.com>
2022-06-06 18:39:36 +0300

    Added Boss Script.

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    [SerializeField] float speedOfBoss;
+    [SerializeField] float stoppingDistance;
+    [SerializeField] float stepBackDistance;
+
+    [SerializeField] Transform player;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance) {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speedOfBoss * Time.deltaTime);
+        } else if (Vector2.Distance(transform.position, player.position) < stoppingDistance
+        && Vector2.Distance(transform.position, player.position) > stepBackDistance) {
+            transform.position = this.transform.position;
+        } else if (Vector2.Distance(transform.position, player.position) < stepBackDistance) {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speedOfBoss * Time.deltaTime);
+        }
+    }
+}
